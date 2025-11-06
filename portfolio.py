a4ceca04682530f7a28ed4dc4871a76e635e2a68
José Mata <josemata@Joses-MacBook-Pro.local>
2025-11-05 23:50:51 -0500

    set app secret key + minor changes

--- portfolio.py
+++ portfolio.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, url_for
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+#this key helps protect the Flask app by securing session data and preventing tampering or forgery of cookies and other sensitive information.
+app.config["SECRET_KEY"]= os.getenv('SECRET_KEY') 
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("home.html")
 
-@app.route("/portfolio_website")
-def portfolio_website_page():
-    return "<h1> Portfolio Website </h1>"
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
